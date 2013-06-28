19ef506fca875414bdb075189bd8947db21c144e
Drup <drupyog@zoho.com>
2013-06-28 05:59:20 +0200

    Add a CmdLiner interface to glilis.

--- glilis/glilis.ml
+++ glilis/glilis.ml
@@ -1,56 +1,84 @@
 open Graphic_order
-open Ls_engine
-open Ls_syntax
-open Ls_type
+open Lilis
+open Cmdliner
 
 let get_lsystem file =
-	let bank_ls = lsystem_from_chanel (open_in file) in
-	BatList.iteri (fun i lsys -> Printf.printf "%N: %s\n" (i+1) (lsys.name)) bank_ls ;
-	print_string "Which L-system do you choose ? " ;
-	let lsys = List.nth bank_ls 0 in
-	print_string "Which generation ? " ;
-	lsys, 10
+  let bank_ls = lsystem_from_chanel (open_in file) in
+  let lsys = List.nth bank_ls 0 in
+  lsys
 
+let init_time, print_time =
+  let time = ref (Unix.gettimeofday ()) in
+  let init () = time := Unix.gettimeofday () in
+  let print () = Printf.printf "Time elapsed : %f\n%!" (Unix.gettimeofday () -. !time) in
+  init, print
 
-let print_time =
-	let time = ref (Unix.gettimeofday ()) in
-	fun () -> print_float (Unix.gettimeofday () -. !time) ; time := Unix.gettimeofday () ; print_newline()
+let to_gtk (width, height) lstream =
+  let expose area ev =
+    let turtle = new Crayon.turtle (Crayon.Gtk area) in
+    turtle#fill () ;
+    draw turtle (BatEnum.clone lstream) ;
+    turtle#draw () ;
+    true
+  in
+  ignore(GMain.init());
+  let window = GWindow.window ~width ~height ~title:"gLILiS" () in
+  ignore (window#connect#destroy GMain.quit);
 
-let gtk_main lstream =
-	let expose area ev =
-		let turtle = new Crayon.turtle (Crayon.Gtk area) in
-		draw turtle lstream ;
-		turtle#draw () ;
-		print_time () ;
-		true
-	in
-	ignore(GMain.init());
-	let window = GWindow.window ~width:700 ~height:700 ~title:"L-system" () in
-	ignore (window#connect#destroy GMain.quit);
+  let area = GMisc.drawing_area ~packing:window#add () in
+  area#misc#set_double_buffered false;
+  ignore(area#event#connect#expose (expose area));
+  window#show ();
+  GMain.main ()
 
-	let	area = GMisc.drawing_area ~packing:window#add () in
-	area#misc#set_double_buffered false;
-	ignore(area#event#connect#expose (expose area));
-	window#show ();
-	GMain.main ()
+let to_png (width, height) lstream file =
+  let turtle = new Crayon.turtle (Crayon.Picture (width, height)) in
+  turtle#fill() ;
+  draw turtle lstream ;
+  turtle#draw () ;
+  turtle#write file
 
+(** Go go Cmdliner ! *)
 
-let png_main lstream =
-	let turtle = new Crayon.turtle (Crayon.Picture (1000,1000)) in
-	turtle#fill() ;
-	draw turtle lstream ;
-	turtle#draw () ;
-	turtle#write "test.png"
+let bank = 
+  let doc = "Charge the $(docv) file as a Lsystem library" in
+  Arg.(required & pos 0 (some string) None & info [] ~docv:"BANK" ~doc)
 
+let generation = 
+  let doc = "Generate the Lsystem at the n-th generation" in
+  Arg.(required & opt (some int) None & info ["n"] ~docv:"GEN" ~doc)
 
-let _ = 
-  print_time () ;
-  let lsys, i = get_lsystem "bank_lsystem" in
-  print_time () ;
-  print_endline "I'm computing !" ;
-  let lstream = eval_lsys i lsys in
-  print_time () ;
-  print_endline "I'm drawing !" ;
-  png_main lstream ;
-  print_endline "I'm done !" ; 
-  print_time ()
+let size = 
+  let doc = "The size of the image, in pixels" in
+  Arg.(value & opt (pair int int) (700,700) & info ["s"; "size"] ~docv:"SIZE" ~doc)
+
+let bench = 
+  let doc = "Print the time of execution" in
+  Arg.(value & flag & info ["b";"bench"] ~docv:"BENCH" ~doc)
+
+let verbose = 
+  let doc = "Be verbose" in
+  Arg.(value & flag & info ["v"] ~doc)
+
+let png = 
+  let doc = "Write a png in $(docv) instead of opening a window" in
+  Arg.(value & opt (some string) None & info ["png"] ~docv:"FILE" ~doc)
+
+let main n bank size bench verbose png =
+  let lsys = get_lsystem bank in
+  if bench then init_time () ;
+  let lstream = eval_lsys n lsys in
+  if verbose then print_endline "I'm computing and drawing !" ;
+  begin match png with 
+    | Some file -> to_png size lstream file 
+    | None -> to_gtk size lstream
+  end ;
+  if verbose then print_endline "I'm done !" ; 
+  if bench then print_time () ;
+  print_endline "Bye !"
+  
+let main_t = Term.(pure main $ generation $ bank $ size $ bench $ verbose $ png)
+
+let () = 
+  match Term.eval (main_t, Term.info "glilis") with 
+    | `Error _ -> exit 1 | _ -> exit 0
