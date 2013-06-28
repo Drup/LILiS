90c27c4067752fddac1fc35a4f778be4db7e5b6a
Drup <drupyog@zoho.com>
2013-06-29 00:33:02 +0200

    Refactorisation of the various turtles classes for a better mordularity.

--- glilis/graphic_order.ml
+++ glilis/graphic_order.ml
@@ -1,17 +1,54 @@
 
+let pi = 4. *. atan 1.
+
 (** Extract the first element of the argument list and replace it with a default value if the list is empty *)
 let get_value_arg l default = match l with
     [] -> default
   | x::_ -> x
 
-class type turtle = object
-  method turn : float -> unit
-  method move : ?trace:bool -> float -> unit
-  method save_position : unit -> unit
-  method restore_position : unit -> unit
-end
+exception Empty_stack
 
-(** translate some usual symbol of L-system into drawing order *)
+(** Class representing a turtle *)
+class turtle =
+  object
+
+    (** Position of the turtle *)
+    val mutable x = 0.
+    val mutable y = 0.
+
+    method get_pos () = (x,y)
+
+    (** Direction of the turtle *)
+    val mutable direction = 0.
+
+    (** Positions remenbered by the turtle *)
+    val mutable stack = []
+
+    (** Turn the turtle by angle in degrees *)
+    method turn angle =
+      direction <- direction +. angle *. pi /. 180.
+
+    (** Move the turtle by d *)
+    method move ?(trace=true) d =
+      x <- x +. (d *. cos direction) ;
+      y <- y +. (d *. sin direction)
+
+    (** Save the position of the turtle in the stack *)
+    method save_position () =
+      stack <- (x,y,direction)::stack
+
+    (** Restore the position of the turtle from the stack. 
+	@raise Empty_Stack if the stack is empty. *)
+    method restore_position () = match stack with
+      | [] -> raise Empty_stack
+      | (new_x,new_y,new_dir)::t -> (
+	  x <- new_x ; y <- new_y ; direction <- new_dir
+	)
+  end
+
+(** Translate some usual symbol of L-system into drawing order.
+    @return a couple (trace, p), if trace is true, then a line to p must be traced.
+*)
 let draw (t : #turtle) order = match order with
   | ("+",l) -> let x = (get_value_arg l 90.) in t#turn (-.x)
   | ("-",l) -> let x = (get_value_arg l 90.) in t#turn x
@@ -19,9 +56,7 @@ let draw (t : #turtle) order = match order with
   | ("B",l) -> let x = (get_value_arg l 1.) in t#move (-. x)
   | ("[",_) -> t#save_position ()
   | ("]",_) -> t#restore_position ()
-  | ("f",l) -> 
-      let x = (get_value_arg l 1.) in 
-      t#move ~trace:false x
+  | ("f",l) -> let x = (get_value_arg l 1.) in t#move ~trace:false x
   | _ -> ()
 
 let draw_enum turtle = BatEnum.iter (draw turtle)
--- glilis/ls_cairo.ml
+++ glilis/ls_cairo.ml
@@ -1,54 +1,20 @@
-exception Empty_stack
+
 exception Not_image
 
-let pi = 4. *. atan 1.
+(** Cairo version of the turtle *)
+class cairo_turtle size_x size_y context surface =
+  object inherit Graphic_order.turtle as super
 
-
-(** Cairo implementation of the turtle *)
-
-
-(** Class representing a turtle *)
-class turtle size_x size_y context surface =
-  object
-
-    (** Position of the turtle *)
-    val mutable x = 0.
-    val mutable y = 0.
-
-    (** Direction of the turtle *)
-    val mutable direction = 0.
-
-    (** Positions remenbered by the turtle *)
-    val mutable stack = []
-
-    (** Turn the turtle by angle in degrees *)
-    method turn angle =
-      direction <- direction +. angle *. pi /. 180.
-
-    (** Move the turtle by d *)
     (* We do the scaling and the rounding by ourself here because cairo do it too slowly *)
     method move ?(trace=true) d =
-      x <- x +. (d *. cos direction) ;
-      y <- y +. (d *. sin direction) ;
-      if trace
-      then (
-	Cairo.line_to context (floor (size_x *. x)) (floor (size_y *. y)) ;
-	Cairo.stroke context
-      ) ;
+      super#move ~trace d ;
+      if trace 
+      then Cairo.line_to context (floor (size_x *. x)) (floor (size_y *. y))
+      else Cairo.move_to context (floor (size_x *. x)) (floor (size_y *. y))
+
+    method restore_position () = 
+      super#restore_position () ;
       Cairo.move_to context (floor (size_x *. x)) (floor (size_y *. y))
-
-    (** Save the position of the turtle in the stack *)
-    method save_position () =
-      stack <- (x,y,direction)::stack
-
-    (** Restore the position of the turtle from the stack, raise Empty_Stack if the stack is empty *)
-    method restore_position () = match stack with
-	[] -> raise Empty_stack
-      | (new_x,new_y,new_dir)::t -> (
-	  Cairo.move_to context (floor (size_x *. new_x)) (floor (size_y *. new_y)) ;
-	  stack <- t ;
-	  x <- new_x ; y <- new_y ; direction <- new_dir
-	)
 
     (** Fill the picture with solid white and set the color to solid black *)
     method fill () =
@@ -57,7 +23,9 @@ class turtle size_x size_y context surface =
       Cairo.set_source_rgba context 0. 0. 0. 1.
 
     (** Apply drawing on the surface *)
-    method draw () = Cairo.stroke context ; Cairo.move_to context (floor (size_x *. x)) (floor (size_y *. y))
+    method draw () = 
+      Cairo.stroke context ; 
+      Cairo.move_to context (floor (size_x *. x)) (floor (size_y *. y))
 
   end
   
@@ -68,7 +36,7 @@ class png_turtle size_x size_y =
   let ctx = Cairo.create surface in
   let _ = Cairo.set_line_width ctx 1. in
   
-  object inherit turtle (float size_x) (float size_y) ctx surface
+  object inherit cairo_turtle (float size_x) (float size_y) ctx surface
 	
     (** Draw to a png *)
     method write file =
@@ -83,6 +51,6 @@ class gtk_turtle w =
   let surface = Cairo.get_target ctx in
   let _ = Cairo.set_line_width ctx 1. in
   
-  object inherit turtle (float size_x) (float size_y) ctx surface
+  object inherit cairo_turtle (float size_x) (float size_y) ctx surface
 
   end 
