9163a1fb2e924d1f5fc3ff6e86555442b47d07eb
Drup <drupyog@zoho.com>
2014-07-02 03:09:15 +0200

    Update packaging and CI script.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,12 +1,10 @@
 # Edit this for your own project dependencies
-OPAM_DEPENDS="ocamlfind oasis batteries menhir cppo cmdliner lablgtk cairo2 tyxml sequence js_of_ocaml"
+PACKAGE=lilis
 
+OPAM_VERSION=1.1.0
 case "$OCAML_VERSION,$OPAM_VERSION" in
-3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
 3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
-4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
 4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
-4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
 4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
 *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
 esac
@@ -15,7 +13,6 @@ echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 export OPAMYES=1
-export OPAMVERBOSE=1
 echo OCaml version
 ocaml -version
 echo OPAM versions
@@ -23,9 +20,15 @@ opam --version
 opam --git-version
 
 opam init
-opam install ${OPAM_DEPENDS}
 eval `opam config env`
-./configure \
-  --enable-executable \
-  --enable-js-of-ocaml
-make
+
+opam pin --verbose ${PACKAGE} .
+
+#opam install --deps-only ${PACKAGE}
+#Doesn't work with opam 1.1 for some reasons
+
+opam install ocamlfind batteries menhir sequence cppo oasis
+
+[ ${EXTRA_DEPENDS} ] && opam install ${EXTRA_DEPENDS}
+opam install --verbose ${PACKAGE}
+opam remove --verbose ${PACKAGE}
